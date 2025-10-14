42b52a753258950f029323d73cc6a19cead6162d
pedro marchese <pedro.marchese03@gmail.com>
2025-10-14 19:59:43 -0300

    feat: card 4 realzado

--- src/components/Ex4.jsx
+++ src/components/Ex4.jsx
@@ -6,7 +6,11 @@ export default function Ex4() {
   return (
     <div className="my-8">
         <h1 className="text-2xl font-bold mb-4">Ex4 - Card Responsivo</h1>
-      {/* Resolva aqui */}
+      <div className="w-full lg:w-96 flex flex-col items-center">
+        <h2>title</h2>
+        <p>text</p>
+        <button className="bg-blue-500 text-white p-2 rounded-full hover:bg-blue-950 text-center flex"> my button</button>
+      </div>
     </div>
   );
 }
