4a2eda34a73f5c616469457eea4ee7ca1b5e50d3
pedro marchese <pedro.marchese03@gmail.com>
2025-10-14 20:07:35 -0300

    feat: ex5 feito

--- src/components/Ex5.jsx
+++ src/components/Ex5.jsx
@@ -9,7 +9,13 @@ export default function Ex5() {
   return (
     <div className="my-8">
         <h1 className="text-2xl font-bold mb-4">Ex5 - Navbar Responsiva</h1>
-      {/* Resolva aqui */}
+      <nav className="w-full bg-gray-400">
+        <ul className="flex flex-col md:flex-row justify-center p-5 gap-4">
+          <li><a href="">link-1</a></li>
+          <li><a href="">link-2</a></li>
+          <li><a href="">link-3</a></li>
+        </ul>
+      </nav>
     </div>
   );
 }
