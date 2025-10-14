bea9ddee87e4f32a3c425e38323c10ef233e0d15
pedro marchese <pedro.marchese03@gmail.com>
2025-10-14 20:25:45 -0300

    feat: ex 7 realizado

--- src/components/Ex7.jsx
+++ src/components/Ex7.jsx
@@ -10,15 +10,15 @@ export default function Ex7() {
   return (
     <div className="my-8">
         <h1 className="text-2xl font-bold mb-4">Ex7 - Dashboard Simples</h1>
-      <header>
+      <header className="w-full justify-between flex">
         <h1>Meu Dashboard</h1>
         <button>Sair</button>
       </header>
-      <main>
-        <div>Card 1</div>
-        <div>Card 2</div>
-        <div>Card 3</div>
-        <div>Card 4</div>
+      <main className="grid grid-cols-1 md:grid-cols-2">
+        <div className="border px-2 py-1 justify-center flex shadow-lg rounded-sm">Card 1</div>
+        <div className="border px-2 py-1 justify-center flex shadow-lg rounded-sm">Card 2</div>
+        <div className="border px-2 py-1 justify-center flex shadow-lg rounded-sm">Card 3</div>
+        <div className="border px-2 py-1 justify-center flex shadow-lg rounded-sm">Card 4</div>
       </main>
     </div>
   );
