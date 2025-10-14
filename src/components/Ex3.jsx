9001f81453a92ad180c23f3ccecda4dbf9e8cf3d
pedro marchese <pedro.marchese03@gmail.com>
2025-10-14 19:50:02 -0300

    feat: ex3

--- src/components/Ex3.jsx
+++ src/components/Ex3.jsx
@@ -6,7 +6,12 @@ export default function Ex3() {
   return (
     <div className="my-8">
         <h1 className="text-2xl font-bold mb-4">Ex3 - Grid Simples</h1>
-      {/* Resolva aqui */}
+      <div className="grid grid-cols-2 w-40 h-40">
+        <div className="w-20 h-20 bg-gray-200"> <p>1</p> </div>
+        <div className="w-20 h-20 bg-gray-200"> <p>2</p> </div>
+        <div className="w-20 h-20 bg-gray-200"> <p>3</p> </div>
+        <div className="w-20 h-20 bg-gray-200"> <p>4</p></div>
+      </div>
     </div>
   );
 }
